162847700f12bc297e74b867a2e9a185b09ad441
Vinicius-R-PeoplePy <109100850+Vinicius-R-PeoplePy@users.noreply.github.com>
2022-09-05 17:04:41 -0300

    Add files via upload

---
+++ f-strings/dict.py
@@ -0,0 +1,46 @@
+#Pessoa = ["Nome: x, Idade: 20",
+#     "Nome2: y, Idade2: 30"]
+#print(Pessoa['Nome'], "tem", Pessoa['Idade'])
+#print(len(Pessoa["Nome"]))
+#print(type(Pessoa))
+#print(Pessoa[1])
+
+# Unidades_de_Medida = {'Metro': 'Metro | Símbolo = m | Equivale a 1m',
+                     # 'Milha': 'Milha | Símbolo = mi | Equivale a ~1609m'
+                     # }
+#Consulta = input("Digite um número: ")
+
+#print(type(Unidades_de_Medida))
+
+#for valor in Unidades_de_Medida.values():
+#    print(f'Definição: {valor}')
+#for x in Unidades_de_Medida:
+#    print(x)
+#for i in range(len(Unidades_de_Medida)):
+#    print(Unidades_de_Medida[0]) ----> como utilizar em dict?
+
+#Unidades_de_Medida = {
+#               "1"          :  {
+#                     "Tipo" : "Metro",
+#                   "Símbolo": "m",
+#                "Equivale a": "1m"
+#               },
+#               "2" : {
+#                     "Milha": "Milha",
+#                   "Símbolo": "mi",
+#                "Equivale a": "~1609m"
+#               }
+#}
+
+#Consulta = input("Digite um número: ")
+#                for i in Consulta:
+#                    if x == [0]
+#                       Consulta = Unidades_de_Medida
+#                    print(f"Definição: "{0})
+#print(Consulta)
+#print(Unidades_de_Medida)
+
+
+#print(Consulta) # -> programar input para usuário digitar de 1 até o último index da lista OU digitar Metro, Centímetro, Milha, para definições...
+
+
---
+++ f-strings/formulario.py
@@ -0,0 +1,8 @@
+Nome= "Vinícius"
+Idade= 23
+Peso= 60.99
+Altura= 1.58
+IMC= Peso/(Altura**2)
+
+print("{0} tem {1} anos, {2}m de altura, pesa {3}kg e seu IMC é de {4:0.2f}".format(Nome, Idade, Altura, Peso, IMC))
+print(Nome, "nasceu em 1999")
