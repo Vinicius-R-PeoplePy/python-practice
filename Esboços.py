0ad67b37acda5f863745f93c562f0f0089c0de47
root <vinicius.people.py@gmail.com>
2022-09-05 19:00:20 -0300

    complex numbers

---
+++ Esboços.py
@@ -0,0 +1,29 @@
+#Nome = "M"
+#Altura = 1.85
+#Idade = 23
+#Peso = 77.5
+#IMC = float(Peso/Altura**2)
+#print({0} tem {1} pesa {2} e seu IMC é de {3}).fstring=({Nome}, {Altura}, {Peso}, {Idade}, {Peso}) # <= voltar & refazer 
+
+#from typing import Dict ---> ???...
+
+
+#Dict = ['SI': 'Sistema Internacional de unidades',
+#    'quilograma': 'definido com base na constante de Planck, cujo valor é 6,62 x 10**-34m²kg/s.']
+#Unidades_Info = ("A respeito de medição de unidades...: ")
+#print(Unidades_Info !!! #review dicts, in depth... Also fstrings, tuples, def functions, (i), counter, loops, arrays, etc... # linkar Unidades_Info à alguma sessão do Dict 
+
+#x = 1j 
+#print(type(x)) # <class 'complex'>
+
+#x = 10E2 -> E não configura "complex number". A letra E junto ao número equivale ao símbolo ** de exponenciação. 
+#print(x) # = 1000
+
+#x = -87.7e100
+#print(x) resultado = -8.77e+101 #o que aconteceu neste caso? *pesquisar*... 
+
+x = 1y
+print(type(x)) #-> x = 1y não funciona para tornar isto um tipo de "complex number"; retorna "SyntaxError: invalid syntax"; pois o número só se torna complexo quando acrescido da letra "j"... 
+
+
+
