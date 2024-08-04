ddbdee87e07c2cf5d4782324ef1dc854a4a2bb49
Vinícius.People.Py <109100850+Vinicius-R-PeoplePy@users.noreply.github.com>
2024-08-04 13:01:22 -0300

    Update choose_your_own_adventure.py

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,7 +1,7 @@
 name = input("Type your name: ")
 print("Welcome", name, "to this adventure!")
 
-answer = input("You are on a dirt road, it has come to and end and you can go left or right. Which way would you like to go? ").lower()
+answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
 
 if answer == "left":
     answer = input("You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
